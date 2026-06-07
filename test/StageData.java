f872440eb6eac5dfce0c2a72a6285b2559a8c180
BongKM1130 <asddwda4@gmail.com>
2026-06-07 21:42:43 +0900

    테트리스 미완성

---
+++ test/StageData.java
@@ -0,0 +1,88 @@
+package test;
+
+public class StageData {
+    public static ArtStage[] getStages() {
+        return new ArtStage[] {
+                new ArtStage(
+                        "동양미술",
+                        "미인도",
+                        "신윤복 , 조선 후기 (18세기 말 ~ 19세기 초)",
+                        "마치 초상화처럼 여인의 전신상을 그린 미인도는 신윤복 이전에는 남아 있는 예가 거의 없다. 이 작품은 19세기의 미인도 제작에 있어 전형(典型)을 제시했다는 점에서 미술사적 의의가 크다.",
+                        "자료 출처 : (국가유산포털) https://www.heritage.go.kr/heri/cul/culSelectDetail.do?pageNo=1_1_1_1&ccbaCpno=1121119730000",
+                        "resources/images/east1.jpg",
+                        1000
+                ),
+
+                new ArtStage(
+                        "동양미술",
+                        "화조구자도",
+                        "이암",
+                        "작품 정보: 16세기 중반에 그려진 보물 제1392호 작품으로, 현재 리움미술관 등에 소장되어 있습니다.주요 소재: 화면 위쪽에는 꽃나무와 새, 나비가 있고, 아래쪽에는 세 마리의 귀여운 강아지가 한가롭게 노는 모습이 묘사되어 있습니다.화풍 특징: 강아지의 털을 윤곽선 없이 먹의 농담만으로 부드럽게 표현하는 몰골법 기법이 사용되어 현실감과 귀여움이 잘 살아있습니다.",
+                        "자료출처 : 네이버 블로그 https://blog.naver.com/leesobia/222332606900",
+                        "resources/images/east2.jpg",
+                        1500
+                ),
+
+                new ArtStage(
+                        "동양미술",
+                        "동양미술 작품 3",
+                        "작가 입력",
+                        "동양미술 작품 3에 대한 간단한 설명을 입력하세요.",
+                        "출처 입력",
+                        "resources/images/east3.jpg",
+                        2000
+                ),
+
+                new ArtStage(
+                        "동양미술",
+                        "동양미술 작품 4",
+                        "작가 입력",
+                        "동양미술 작품 4에 대한 간단한 설명을 입력하세요.",
+                        "출처 입력",
+                        "resources/images/east4.jpg",
+                        2500
+                ),
+
+                new ArtStage(
+                        "서양미술",
+                        "모나리자",
+                        "레오나르도 다 빈치",
+                        "르네상스 시대 (16세기 초, 1503~1519년경))\r\n"
+                        + ": 가상의 풍경 앞에 인물을 배치하고 sfumato 기법과 공중 원근법을 본격적으로 구사하여, 인물과 배경을 하나로 융합시킨 초상화라는 점에서 미술사적 의의가 크다",
+                        "자료 출처 :  (ArtMajeur) https://www.artmajeur.com/ko/magazine/5-misulsa/le-onaleudo-dabinchiui-monalija/333478",
+                        "resources/images/west1.jpg",
+                        1000
+                ),
+
+                new ArtStage(
+                        "서양미술",
+                        "생 베르나르 고개의 나폴레옹",
+                        "자크 루이 다비드",
+                        "나폴레옹이 1800년 마렝고 전투 당시 군대를 이끌고 알프스를 넘는 장면을 이상적으로 표현했습니다.백마를 탄 나폴레옹이 오른손을 뻗어 진격을 지시하는 역동적인 모습이 특징이며, 실제로는 노새를 타고 건넜다는 기록과 달리 백마로 묘사되었습니다.스페인 국왕의 의뢰로 제작된 이 그림은 나폴레옹의 영웅적 이미지를 극대화하기 위해 기획되었습니다.",
+                        "자료출처:위키피디아 https://ko.wikipedia.org/wiki/%EC%83%9D%EB%B2%A0%EB%A5%B4%EB%82%98%EB%A5%B4_%EA%B3%A0%EA%B0%9C%EC%9D%98_%EB%82%98%ED%8F%B4%EB%A0%88%EC%98%B9",
+                        "resources/images/west2.jpg",
+                        1500
+                ),
+
+                new ArtStage(
+                        "서양미술",
+                        "서양미술 작품 3",
+                        "작가 입력",
+                        "서양미술 작품 3에 대한 간단한 설명을 입력하세요.",
+                        "출처 입력",
+                        "resources/images/west3.jpg",
+                        2000
+                ),
+
+                new ArtStage(
+                        "서양미술",
+                        "서양미술 작품 4",
+                        "작가 입력",
+                        "서양미술 작품 4에 대한 간단한 설명을 입력하세요.",
+                        "출처 입력",
+                        "resources/images/west4.jpg",
+                        2500
+                )
+        };
+    }
+}
